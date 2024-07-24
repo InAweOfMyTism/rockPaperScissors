0f6553cf2cc31a6aea9067c377bee11214e9203d
Chris Carroll <175367668+InAweOfMyTism@users.noreply.github.com>
2024-07-24 17:19:42 -0400

    added to play round function

--- script.js
+++ script.js
@@ -23,8 +23,26 @@ function getHumanChoice(){
 }
 
 function playRound(humanChoice, computerChoice){
-    let human = humanChoice.toUpperCase();
-    let computer = computerChoice.toUpperCase();
-    switch(human)
+    let human = humanChoice.toUpperCase(); //ensure the human choice is case insensitive
+    let computer = computerChoice.toUpperCase(); //do the same for computer choice
+    switch(human){ //evaluate conditions based on the human input
+        case ROCK:
+            switch(computer){
+                case ROCK:
+                    //tie
+                    break;
+                case PAPER:
+                    //computer Wins! Paper beats Rock
+                    break;
+                case SCISSORs:
+                    break;
+            }
+            break;
+        case PAPER:
+            break;
+        case SCISSORs:
+            break;
+        
+    }
 }
 
