4f76c0ebe7b0dfc66a5b1bee2f72101816967028
Chris Carroll <175367668+InAweOfMyTism@users.noreply.github.com>
2024-07-24 12:25:20 -0400

    added contents and comments to function body for get computer choice

--- script.js
+++ script.js
@@ -2,14 +2,29 @@ console.log("Hello World!");
 
 
 function getComputerChoice(){
-
+    let choice; //create variable to store computer's choice
+    let randNum = Math.floor(Math.random()*3); //generate a random integer between 0-2
+    switch(randNum){ //assign a value to the computer's choice based on radnom int
+        case 0:
+            choice = 'RoCk';
+            break;
+        case 1:
+            choice = 'PapER';
+            break;
+        case 2:
+            choice = 'ScissoRs';
+            break;
+    }
+    return choice; // return the computer choice to the function call
 }
 
 function getHumanChoice(){
-    
+    console.log('Rock, Paper Scissors, SHOOT!'); //prompt user to enter a choice
 }
 
 function playRound(humanChoice, computerChoice){
-
+    let human = humanChoice.toUpperCase();
+    let computer = computerChoice.toUpperCase();
+    switch(human)
 }
 
